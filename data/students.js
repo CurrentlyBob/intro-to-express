eeadc8f029ac1c551fd926b22434a41b59c5322b
BobWa <walshbobby5@gmail.com>
2023-04-11 17:29:04 -0500

    complete lab

---
+++ data/students.js
@@ -0,0 +1,9 @@
+const students = [
+    {name: "bob", age: 23, smelly: false, id: 69420},
+    {name: "connor", age: 25, smelly: true, id: 69421},
+    {name: "lozeno", age: 23, smelly: true, id: 69422},
+]
+
+export {
+    students,
+}
